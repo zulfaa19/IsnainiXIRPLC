cda2b26a6ad4beb5a3cf7cdbfedfb1cb81f8ff73
zulfaa19 <114550637+zulfaa19@users.noreply.github.com>
2022-09-28 10:26:41 +0700

    membuat file info.php

---
+++ info.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<title>Website Check Page</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>We&rsquo;re Jagoan Cloud!</h1>
+    <div>
+        <p>Helloo SMKN 4 Malang, this is check page only!</p>
+        <p>Isnaini Zulfa - XIRPLC</p>
+        <p>Surya Wardhana - XIRPL2</p>
+        <p>&mdash; Thanks</p>
+        
+    </div>
+</article>
